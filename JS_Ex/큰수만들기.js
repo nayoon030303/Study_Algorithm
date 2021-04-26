bd90cd4f0f6418c14c15bb695c315fb366ba69ac
nayoon030303 <nayoon030303@gmail.com>
2021-04-26 16:05:45 +0900

    큰수만들기_v1

--- JS_Ex/큰수만들기.js
+++ JS_Ex/큰수만들기.js
@@ -1,13 +1,37 @@
 function solution(number, k) {
     var len = number.length-k;
-    number = number.split('');
+    number = number.split('').map((content)=>Number(content));
     for(var i=0; i<number.length; i++){
-        if(number[i]<number[i+1] && number.length>len){
-            number = number.filter((content,index)=>index!=i);
-            i = -1;
+        if(k<=0)break;
+        if(number[i]===9){
+            i = i+1;
+            break;
+        };
+        if((number[i]<number[i+1]|| number[i+1]===undefined) && k>0){
+            number.splice(i,1);
+            k-=1;
+            i=-1;
         }
     }
     
-    console.log(number);
+    console.log(number.length);
     return number.join('');
 }
+number = '9999';
+console.log(number.length);
+k = 2;
+console.log(solution(number,k));
+
+// if(number[i]===9 && i<len-1){
+//     console.log('break');
+//     continue;
+// }
+// //console.log(number[i],number[i+1]);
+// if(number[i]<number[i+1] && number.length>len){
+//     number.splice(i,1);
+//     i = -1;
+// }else if(number[i] ===number[i+1] && number.length>len){
+//    // console.log('a');
+//     number.splice(i,1);
+//     i = -1;
+// }
