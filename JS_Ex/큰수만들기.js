31f70c97d5147f965a68191b04441b69daf0bbe8
nayoon030303 <nayoon030303@gmail.com>
2021-04-26 20:55:09 +0900

    answer: stack

--- JS_Ex/큰수만들기.js
+++ JS_Ex/큰수만들기.js
@@ -2,36 +2,23 @@ function solution(number, k) {
     var len = number.length-k;
     number = number.split('').map((content)=>Number(content));
     for(var i=0; i<number.length; i++){
+      
         if(k<=0)break;
-        if(number[i]===9){
-            i = i+1;
-            break;
-        };
-        if((number[i]<number[i+1]|| number[i+1]===undefined) && k>0){
+
+        //current가 current+1보다 작으면 
+        if((number[i]<number[i+1]|| number[i+1]===undefined)){
+
+            //current 삭제
             number.splice(i,1);
             k-=1;
             i=-1;
         }
     }
     
-    console.log(number.length);
     return number.join('');
 }
-number = '9999';
-console.log(number.length);
-k = 2;
-console.log(solution(number,k));
+        
+number = '4177252841';
 
-// if(number[i]===9 && i<len-1){
-//     console.log('break');
-//     continue;
-// }
-// //console.log(number[i],number[i+1]);
-// if(number[i]<number[i+1] && number.length>len){
-//     number.splice(i,1);
-//     i = -1;
-// }else if(number[i] ===number[i+1] && number.length>len){
-//    // console.log('a');
-//     number.splice(i,1);
-//     i = -1;
-// }
+k = 4;
+console.log(solution(number,k));
---
+++ JS_Ex/큰수만들기_answer.js
@@ -0,0 +1,24 @@
+function solution(number, k) {
+   const stack = [];
+   let answer = '';
+
+   for(let i=0; i<number.length; i++){
+       const element = number[i];
+
+       //k가 0보다 크면서 element가 stack의 마지막값 보다 크면
+       while(k>0 && stack[stack.length-1]<element){
+           stack.pop();//마지막 값을 뺸다.
+           k--;
+       }
+       stack.push(element);
+   }
+
+   console.log("an:"+stack);
+   stack.splice(stack.length-k,k);
+   answer = stack.join('');
+   return answer;
+}
+        
+number = '99991';
+k = 2;
+console.log(solution(number,k));
