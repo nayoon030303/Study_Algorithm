b1dba1ecbe06f53372ca68b530ce4a652d571d6d
nayoon030303 <nayoon030303@gmail.com>
2021-05-19 23:10:14 +0900

    add commet

--- JS_Ex/약수의개수와덧셈.js
+++ JS_Ex/약수의개수와덧셈.js
@@ -24,7 +24,7 @@ function solution(left, right) {
  function solution(left, right) {
      var answer = 0;
      for (let i = left; i <= right; i++) {
-         if (Number.isInteger(Math.sqrt(i))) {
+         if (Number.isInteger(Math.sqrt(i))) { //제곱근이 정수면, 약수의 갯수가 홀수
              answer -= i;
          } else {
              answer += i;
