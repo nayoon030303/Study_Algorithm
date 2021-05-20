9ba597d577ee988f131cc22f8eab24606545b2bf
nayoon030303 <nayoon030303@gmail.com>
2021-05-20 20:12:56 +0900

    백준11399

---
+++ backjune/Algorithm/Algorithm/Algorithm.cpp
@@ -0,0 +1,36 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long num;
+    cin >> num;
+    long long* times = new long long[num];
+    for (int i = 0; i < num; i++) {
+        cin >> times[i];
+    }
+
+    //오름차순으로 정렬
+    for (int i = 0; i < num-1; i++) {
+        for (int j = i+1; j < num; j++) {
+            if (times[i] > times[j]) {
+                int temp = times[i];
+                times[i] = times[j];
+                times[j] = temp;
+            }
+        }
+    }
+    int sum = 0;
+    for (int i = 0; i < num; i++) {
+        for (int j = 0; j <= i; j++) {
+            sum += times[j];
+        }
+        //cout<< times[i];
+    }
+    cout << sum << endl;
+    
+    return 0;
+}
